b778c8996d7e76bf163062641223d7b331f96a83
SilvaInac <eduardo040silva@gmail.com>
2022-11-29 16:49:02 -0300

    add mongoose

---
+++ 6_API/src/infrastructure/database.js
@@ -0,0 +1,30 @@
+const monoose = require('mongoose');
+
+const uri = 'mongodb+srv://user_c214:LQbM3cK8nFwocN4J@cluster0.zjmah4e.mongodb.net/?retryWrites=true&w=majority'
+
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+    id: {
+        type: String,
+        index: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        unique: true,
+    },
+    nome: String,
+    senha: String,
+});
+
+const UserModel = mongoose.model('UserModel', UserSchema);
+
+module.exports = {
+    UserModel,
+};
